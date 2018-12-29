771fb55beecb8495f869b46bfe862d1a7941b497
AhmadHaleeem <coldman2099@gmail.com>
2018-12-29 20:51:10 +0100

    Making our First API Calls with Axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-// Global app controller
+import axios from 'axios'
 
-import num from './test'
-const x = 23;
-console.log(`I imported ${num} from another module called test.js variable x is ${x}`);
+async function getResult(query) {
+    const key = '78f60614377026eba6720d4b216d23a0';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (e) {
+        alert(e)
+    }
+
+}
+
+getResult('pizza');
+
+// https://www.food2fork.com/api/search
+// 78f60614377026eba6720d4b216d23a0
